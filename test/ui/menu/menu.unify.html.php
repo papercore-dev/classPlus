d9498ea7f47f848eca05de2b12acbe6084fcf138
papercore-dev <gyubini38@gmail.com>
2023-02-26 21:30:52 +0000

    메일 주소, 프로필 표시

--- test/ui/menu/menu.unify.html.php
+++ test/ui/menu/menu.unify.html.php
@@ -16,11 +16,21 @@ chdir(dirname(__FILE__));?>
 </div>
 <div class="flex">
 
-<div class="rounded-full text-gray-500 mr-2 mt-2 hover:bg-gray-100">
-<img class="w-8 h-8 rounded-full object-cover" src="
-      https://cdn.discordapp.com/attachments/924239126377685015/1067231154656256050/image.png
-      " alt="avatar"/>
-
+<div @click.away="open = false" class="relative" x-data="{ open: false }">
+<button @click="open = !open" class="rounded-full text-gray-500 mr-2 mt-2 hover:bg-gray-100">
+<img class="w-8 h-8 rounded-full object-cover" src="<?php echo getData('userAvatar'); ?>" alt="avatar">
+</button>
+<div style="z-index: 9999;" x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+<div class="px-2 py-2 bg-white rounded-md shadow dark:bg-gray-800 text-black dark:text-white">
+<a class="hover:bg-blue-500 hover:text-white rounded-lg block px-4 py-2 mt-2 bg-transparent text-sm" href="#">
+  <strong><?php echo getData('userName'); ?></strong>님<br>
+  <span class="text-xs"><?php echo getData('userID'); ?></span>
+</a>
+<a class="hover:bg-blue-500 hover:text-white rounded-lg block px-4 py-2 mt-2 bg-transparent text-sm" href="javascript:Turbo.visit(`/oauth/logout.php`);">
+  로그아웃
+</a>
+</div>
+</div>
 </div>
 
 <a href="javascript:Turbo.visit(`/notice.php`);">
@@ -33,15 +43,3 @@ chdir(dirname(__FILE__));?>
 </a>
 </div>
 </div>
-
-
-<script>
-  function logoutConfirm(){
-    if (confirm("로그아웃 하시겠어요?") == true){    //확인
-      Turbo.visit(`/logout.php`);
-    }
-    else{   //취소
-      return false;
-    }
-  }
-</script>
