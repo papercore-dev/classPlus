d8c36426a72a961d1b0f8364e97ed0888f92780d
papercore-dev <gyubini38@gmail.com>
2023-02-26 22:19:01 +0000

    구버전 롤백

--- test/ui/menu/menu.unify.html.php
+++ test/ui/menu/menu.unify.html.php
@@ -3,10 +3,15 @@ chdir(dirname(__FILE__));
 include '../../security.php';
 chdir(dirname(__FILE__));?>
 <div class="max-w-md flex items-center justify-between">
-<div class="p-2 text-gray-500 ">
-<a href="javascript:Turbo.visit(`/index.php`)">
-<h1 class="text-lg text-gray-500">
-<img src="/resources/images/logo_gray.png" class="h-6 inline-block opacity-75"> | <?php echo getData('schoolName'); ?></h1>
+<div class="p-3 text-gray-500 ">
+  <a href="javascript:Turbo.visit(`/index.php`)">
+<h1 class="text-2xl font-bold text-black">
+  <img src="/resources/images/logo_gray.png" class="h-4 opacity-75">
+  <?php
+  echo getData('schoolName').' '.getData('schoolGrade').'학년 '.getData('schoolClass').'반';
+  ?>
+  
+</h1>
 </a>
 </div>
 <div class="flex">
@@ -15,7 +20,7 @@ chdir(dirname(__FILE__));?>
 <button @click="open = !open" class="rounded-full text-gray-500 mr-2 mt-2 hover:bg-gray-100">
 <img class="w-8 h-8 rounded-full object-cover" src="<?php echo getData('userAvatar'); ?>" alt="avatar">
 </button>
-<div style="z-index: 9999;" class="opacity-0" x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+<div style="z-index: 9999;" x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
 <div class="px-2 py-2 bg-white rounded-md shadow dark:bg-gray-800 text-black dark:text-white">
 <a class="hover:bg-blue-500 hover:text-white rounded-lg block px-4 py-2 mt-2 bg-transparent text-sm" href="#">
   <strong><?php echo getData('userName'); ?></strong>님<br>
