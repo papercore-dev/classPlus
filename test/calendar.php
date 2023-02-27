85dcbb53ea8cd5f6f1df33ac92fba0491fbbe632
papercore-dev <gyubini38@gmail.com>
2023-02-27 16:23:53 +0000

    캘린더 디자인 변경 및 실제 달력 반영

--- test/calendar.php
+++ test/calendar.php
@@ -19,36 +19,36 @@ chdir(dirname(__FILE__));
 include 'ui/menu/menu.tl.html.php';
 chdir(dirname(__FILE__));
 ?>
-<div class="my-4">
+<div class="my-4 p-5">
 <?php
 //show this month's calendar
 function showCalendar($month, $year){
     $firstDay = mktime(0,0,0,$month, 1, $year);
-    $title = date('M', $firstDay);
+    $title = date('m', $firstDay);
     $dayOfWeek = date('D', $firstDay);
     $daysInMonth = cal_days_in_month(0, $month, $year);
     $blank = date('w', strtotime("{$year}-{$month}-01"));
     $dayCount = 1;
     $today = date('Y-m-d');
     $calendar = "<table class='table-auto w-full'>
+    <div class='mb-5 flex items-center justify-between'>
+<h4 class='text-2xl font-bold text-slate-500'>$year 년 $title 월</h4>
+</div>
     <tr>
-    <th colspan='7' class='text-center text-xl font-bold text-gray-900 dark:text-white'>$year 년 $title 월</th>
-    </tr>
-    <tr>
-    <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>일</td>
+    <td class='w-1/7 text-center text-lg  text-red-500'>일</td>
     <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>월</td>
     <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>화</td>
     <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>수</td>
     <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>목</td>
     <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>금</td>
-    <td class='w-1/7 text-center text-lg  text-gray-900 dark:text-white'>토</td>
+    <td class='w-1/7 text-center text-lg  text-blue-500'>토</td>
     </tr>
     <tr>";
     $calendar .= "<td colspan='$blank'>&nbsp;</td>";
     while($dayCount <= $daysInMonth){
         $calendar .= "<td class='text-center text-lg  text-gray-900 dark:text-white'>";
         if($dayCount == date('d') && $month == date('m') && $year == date('Y')){
-            $calendar .= "<div class='bg-blue-500 rounded-full w-8 h-8 flex items-center mx-auto justify-center text-white'>$dayCount</div>";
+            $calendar .= "<div class='bg-blue-200 rounded-full w-8 h-8 flex items-center mx-auto justify-center text-blue-500'>$dayCount</div>";
         } else {
             $calendar .= $dayCount;
         }
@@ -62,7 +62,8 @@ function showCalendar($month, $year){
     echo $calendar;
 }
 
-showCalendar(2, 2023);
+//use showCalendar function to show this month's calendar
+showCalendar(date('m'), date('Y'));
 ?>
 </div>
 <?php
