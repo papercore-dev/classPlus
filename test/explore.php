ab2f0538f72b838362c24c531f90253f5d6d0d0f
papercore-dev <gyubini38@gmail.com>
2023-02-28 17:03:05 +0000

    검색 기능 적용

--- test/explore.php
+++ test/explore.php
@@ -20,21 +20,53 @@ $serviceName = "boardexplore";
 include 'functions/analyzeLogs.php';
 chdir(dirname(__FILE__));
 ?>
+<?php
+//check if POST's search is set
+if (isset($_POST['search'])){
+    $search = $_POST['search'];
+    //only accept A-Z, a-z, Korean, 0-9, and space
+    if (!preg_match("/^[가-힣a-zA-Z0-9 ]*$/", $search)){
+        $search = "";
+    }
+}
+else{
+    $search = "";
+}
+?>
 <section class="p-5">
 <div class="mb-5 flex items-center justify-between">
-<h4 class="text-2xl font-bold text-slate-500">탐색</h4>
+<h4 class="text-2xl font-bold text-slate-500">
+    <?php
+    if ($search == ""){
+        echo "탐색";
+    }
+    else{
+        echo $search." 검색 결과";
+    }
+    ?>
+</h4>
 </div>
 
-<form>   
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+<form action="/explore.php" method="post">   
+        <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
         <div class="relative">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-            <input type="search" id="default-search" class="block p-2 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="검색" required>
+            <input type="search"     <?php
+    if ($search == ""){
+        echo "value=''";
+    }
+    else{
+        echo "value='".$search."'";
+    }
+    ?> id="search" name="search" class="block p-2 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="검색" required>
         </div>
 </form>
 
+<?php
+    if ($search == ""){
+        echo'
 <div class="mt-4 relative flex flex-col min-w-0 break-words w-full">
 
 
@@ -42,13 +74,11 @@ chdir(dirname(__FILE__));
         <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white"><span class="tossface">🔥</span>&nbsp;HOT 게시판</h3>
    </div>
    <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <?php
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">';
             $getCommunityList = "SELECT * FROM `posts_board` WHERE `boardHidden` = '0' AND `view_accessLevel` <= ".getData("accessLevel")." ORDER BY `visitCount` DESC";
             include 'functions/listRank.php';
             chdir(dirname(__FILE__));
-            ?>
-
+    echo'
         </ul>
           </div>
 
@@ -56,17 +86,33 @@ chdir(dirname(__FILE__));
         <h3 class="text-xl font-bold leading-none text-gray-900 dark:text-white"><span class="tossface">✨</span>&nbsp;NEW 게시판</h3>
    </div>
    <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <?php
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">';
             $getCommunityList = "SELECT * FROM `posts_board` WHERE `boardHidden` = '0' AND `view_accessLevel` <= ".getData("accessLevel")." ORDER BY `boardID` DESC";
             include 'functions/listRank.php';
             chdir(dirname(__FILE__));
-            ?>
+            echo'
 
         </ul>
           </div>
         </div>
-</section>
+</section>';
+    }
+    else{
+        echo'
+        <div class="mt-4 relative flex flex-col min-w-0 break-words w-full">
+        
+           <div class="flow-root">
+                <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">';
+                    $getCommunityList = "SELECT * FROM `posts_board` WHERE `boardHidden` = '0' AND `view_accessLevel` <= ".getData("accessLevel")." AND `boardName` LIKE '%".$search."%' ORDER BY `boardID` DESC";
+                    include 'functions/listRank.php';
+                    chdir(dirname(__FILE__));
+                    echo'
+                    </ul>
+                      </div>
+                    </div>
+            </section>';
+    }
+?>
 <?php
 include 'ui/common/footer.html.php';
 chdir(dirname(__FILE__));
