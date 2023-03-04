8f7a94baa15435624b66d84aba1031d63c92b9d3
papercore-dev <gyubini38@gmail.com>
2023-03-04 16:41:34 +0000

    맛있는 급식 볼드 처리

--- test/ftable.php
+++ test/ftable.php
@@ -58,7 +58,7 @@ chdir(dirname(__FILE__));
       //current date in YYYYMMDD format
       $currentDate = date("Ymd");
       //7 days later date in YYYYMMDD format
-      $sevenDaysLater = date("Ymd", strtotime("+7 days"));
+      $sevenDaysLater = date("Ymd", strtotime("+1 month"));
       //get school meal data
       $mealData = checkNEIS("hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=".$schoolRegionCode."&SD_SCHUL_CODE=".getData("schoolSID")."&MLSV_FROM_YMD=".$currentDate."&MLSV_TO_YMD=".$sevenDaysLater."&Type=json")["mealServiceDietInfo"][1]["row"];
 
@@ -91,18 +91,36 @@ chdir(dirname(__FILE__));
             break;
         }
 
+        if ($day["MMEAL_SC_NM"] == "조식"){
+          $mealType = "아침";
+        } else if ($day["MMEAL_SC_NM"] == "중식"){
+          $mealType = "점심";
+        } else if ($day["MMEAL_SC_NM"] == "석식"){
+          $mealType = "저녁";
+        }
+        else{
+          $mealType = $day["MMEAL_SC_NM"];
+        }
+        
         echo '<div class="my-5 flex items-center justify-between">
-        <h4 class="text-2xl font-bold text-slate-500">'.$beautifiedDate.' '.$dayOfWeek.' '.$day["MMEAL_SC_NM"].'</h4><p class="text-gray-500">'.$day["CAL_INFO"].'</p>
+        <h4 class="text-2xl font-bold text-slate-500">'.$beautifiedDate.' '.$dayOfWeek.'</h4><p class="text-gray-500">'.$day["CAL_INFO"].'</p>
         </div>';
         //if DDISH_NM includes word in array, make it bold
-        $tastyFood = array("돼지고기", "소고기", "닭고기", "오리고기", "양고기", "계란", "쌀", "콩", "밀", "잡곡", "콩나물", "콩나물국");
+        $tastyFood = array("스테이크", "고기", "빵", "요거트", "요구르트", "돈가스", "돈까스", "튀김", "볶음", "짜장", "짬뽕", "우동", "주스", "쥬스", "스무디", "라떼", "아이스크림", "떡볶이", "김밥", "케이크");
         $dishToday = $day["DDISH_NM"];
         foreach($tastyFood as $food){
           if(strpos($day["DDISH_NM"], $food) !== false){
             $dishToday = str_replace($food, "<b>".$food."</b>", $day["DDISH_NM"]);
           }
         }
-        echo $dishToday;
+        echo $mealType.'<br><br>'.$dishToday;
+        if ($dayOfWeek == "금요일"){
+          echo '<div class="mt-4 flex items-center justify-between">
+          <span class="border-b w-1/5 lg:w-1/4"></span>
+          <a href="#" class="text-xs text-center text-gray-500 uppercase">한 주 끝!</a>
+          <span class="border-b w-1/5 lg:w-1/4"></span>
+      </div>';
+        }
       }
       ?>
 </div>
