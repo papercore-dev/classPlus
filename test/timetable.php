739338e540b3b3e0cb57691675401c3d0594185a
papercore-dev <gyubini38@gmail.com>
2023-03-04 11:28:54 +0000

    시간표 디자인 개선

--- test/timetable.php
+++ test/timetable.php
@@ -83,18 +83,22 @@ chdir(dirname(__FILE__));
         $timetable = json_decode($timetable, true);
         $studytime = json_decode($studytime, true);
         //create table to show timetable (get row from number of array in $timetable and get column from largest number of array in $timetable[n])
-        echo "<table class='w-full mx-4 table-auto border border-gray-100 bg-gray-50 p-1'>";
+        echo "<table class='w-full table-auto border border-gray-100 bg-gray-50 p-1'>";
         echo "<tr>";
-        echo "<th class='border border-gray-100 bg-gray-50 p-1'>교시</th>";
+        echo "<th class='border border-blue-100 bg-gray-50 p-1'>교시</th>";
         for ($i=0; $i < count($timetable); $i++) {
           //show week of day
           $weekofday = array("월", "화", "수", "목", "금", "토", "일");
-          echo "<th class='border border-gray-100 bg-gray-50 p-1'>".$weekofday[$i]."</th>";
+          echo "<th class='border border-blue-100 bg-gray-50 p-1'>".$weekofday[$i]."</th>";
         }
         echo "</tr>";
         for ($i=0; $i < count($timetable[0]); $i++) {
+          $beautifiedTime = $studytime[$i];
+          //replace ( to <br>( to beautify time
+          $beautifiedTime = str_replace("(", "교시<br><span class='text-xs'>(", $beautifiedTime);
+          $beautifiedTime = str_replace(")", ")</span>", $beautifiedTime);
           echo "<tr>";
-          echo "<td class='border border-gray-100 bg-gray-50 p-1 text-center'>".($i+1)."교시</td>";
+          echo "<td class='border border-blue-100 bg-gray-50 p-1 text-center'>".$beautifiedTime."</td>";
           for ($j=0; $j < count($timetable); $j++) {
             echo "<td class='border border-gray-100 bg-gray-50 p-1 text-center'>".$timetable[$j][$i]["subject"]."<br>".$timetable[$j][$i]["teacher"]."</td>";
           }
