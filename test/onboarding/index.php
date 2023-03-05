9bc56b8a63b50b481c0f5ae6f7b9396d8c1dc02a
papercore-dev <gyubini38@gmail.com>
2023-03-05 16:34:34 +0000

    홍보영상 추가

--- test/onboarding/index.php
+++ test/onboarding/index.php
@@ -151,6 +151,7 @@ _INTERVAL_VAL = setInterval(Type, 100);
         </label>
 	</div>
 </div>
+<div style="width:100%;height:0px;position:relative;padding-bottom:56.250%;"><iframe src="https://streamable.com/e/opc1mp?autoplay=1&amp;nocontrols=1" frameborder="0" width="100%" height="100%" allowfullscreen="" allow="autoplay" style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;" class="rounded-xl"></iframe></div>
 </section>
 <div class="pb-32"></div>
 
