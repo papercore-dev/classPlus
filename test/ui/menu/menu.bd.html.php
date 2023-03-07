397cead83ee2cf23c8cb8a5ed5d5184257c5d0b0
papercore-dev <gyubini38@gmail.com>
2023-03-07 18:29:42 +0000

    글 고정 및 삭제

--- test/ui/menu/menu.bd.html.php
+++ test/ui/menu/menu.bd.html.php
@@ -10,7 +10,7 @@ chdir(dirname(__FILE__));?>
 <div class="bg-gray-100 rounded-b-xl py-2  text-white">
 
 <div class="max-w-md flex items-center justify-between"><a class="rounded-full text-gray-900 p-1 ml-2 hover:bg-gray-200"
-href="javascript:history.back()">
+href="javascript:Turbo.visit(`app.php`)">
 <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
 </a>
 
