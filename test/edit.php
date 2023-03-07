35f3be6af2e4ed5c7d6b48bebaf009cad3282b74
papercore-dev <gyubini38@gmail.com>
2023-03-07 18:44:57 +0000

    글 수정

---
+++ test/edit.php
@@ -0,0 +1,162 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include 'ui/common/header.html.php';
+chdir(dirname(__FILE__));
+
+include 'functions/checkAccount.php';
+chdir(dirname(__FILE__));
+requireSignin("/menu.php");
+requireStdVerification();
+
+include 'functions/checkUserData.php';
+chdir(dirname(__FILE__));
+include 'functions/timeToRelative.php';
+chdir(dirname(__FILE__));
+
+//ID 검증
+if (!isset($_GET["id"])){
+    echo "<script>window.location.href = '/explore.php?error=게시판이 존재하지 않거나 삭제됐어요.';</script>";
+    die;
+}
+if (!is_numeric($_GET["id"])){
+    echo "<script>window.location.href = '/explore.php?error=게시판이 존재하지 않거나 삭제됐어요.';</script>";
+    die;
+} 
+
+//get board id from post id
+$getBoardID = "SELECT boardID FROM `posts` WHERE postID = '".$_GET["id"]."' AND postHidden = '0' AND userID = '".$_SESSION['userID']."' AND signMethod = '".$_SESSION['signMethod']."'";
+$getBoardID_Result = $db->query($getBoardID);
+if ($getBoardID_Result->rowCount() == 0){
+    echo "<script>window.location.href = '/explore.php?error=게시판이 존재하지 않거나 삭제됐어요.';</script>";
+    die;
+}
+else{
+    while($row = $getBoardID_Result->fetch()){
+        $boardID = $row["boardID"];
+        $title = $row["postTitle"];
+        $content = $row["postContent"];
+        $image = $row["postAttachment"];
+    }
+}
+
+$serviceName = $boardID;
+
+$getServiceData = "SELECT * FROM `posts_board` WHERE boardID = '".$serviceName."' AND boardHidden = '0' AND write_accessLevel <= '".$_SESSION['accessLevel']."'";
+$getServiceData_Result = $db->query($getServiceData);
+if ($getServiceData_Result->rowCount() == 0){
+    echo "<script>window.location.href = '/explore.php?error=게시판이 존재하지 않거나 삭제됐어요.';</script>";
+    die;
+}
+else{
+    while($row = $getServiceData_Result->fetch()){
+        include 'functions/specificFunction.php';
+        if ($isBannerHidden){
+            echo "<script>window.location.href = '/explore.php?error=게시판이 존재하지 않거나 삭제됐어요.';</script>";
+            die;
+        }
+    }
+}
+
+//게시판 이름 구하기
+$getServiceName = "SELECT * FROM `posts_board` WHERE boardID = '".$serviceName."'";
+$getServiceName_Result = $db->query($getServiceName);
+$boardName = "";
+while($row = $getServiceName_Result->fetch()){
+    $boardName = $row["boardName"];
+}
+
+$headName = $boardName." 게시판의 글 수정하기";
+include 'ui/menu/menu.custom.html.php';
+chdir(dirname(__FILE__));
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <div class="mx-auto w-full">
+    <form action="/form/postEdit.php" method="POST">
+      <div class="">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="제목"
+          value="<?php echo $title;?>"
+          class="w-full border-t border-b bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      <div class="mb-5">
+        <textarea
+          rows="15"
+          name="post"
+          id="post"
+          placeholder="내용을 작성해주세요"
+          value="<?php echo $content;?>"
+          class="w-full resize-none border-b bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        ></textarea>
+      </div>
+      <div>
+        <input type="hidden" name="imageURL" id="imageURL" value="<?php echo $image;?>" />
+        <input type="hidden" name="postURL" id="postURL" value="<?php echo $_GET["id"];?>" />
+        <button
+          class="hover:shadow-lg rounded-lg bg-blue-500 py-3 px-8 text-base font-semibold text-white outline-none"
+        >
+          수정하기
+        </button>
+      </div>
+    </form>
+            <!-- image upload form using imgbb -->
+            <div class="mb-5">
+
+<p class="text-red-500 mt-4" id="submissionError">
+</p>
+<img src="<?php echo $image;?>" id="preview" class="w-1/2 rounded-lg">
+<input type="file" id="image" hidden style="display:none;" accept="image/*"></input>
+<button type="button" id="next-button" class="hover:shadow-lg rounded-lg bg-blue-500 py-3 px-8 text-base font-semibold text-white outline-none" onclick="imgUpload()">이미지 업로드</button>
+<script>
+function imgUpload() {
+image = document.getElementById("image");
+image.click();
+image.style.display = "block";
+document.getElementById("next-button").classList.add("opacity-50");
+}
+document.getElementById("image").addEventListener("change", function() {
+    //use nsfwjs to cease upload if image is NSFW
+var form = new FormData();
+    form.append("image", image.files[0]);
+    form.append("key", "f6b9c05f362e92b7be21792346f9243e");
+    form.append("name", "post_upload_<?php echo $_GET['id']; ?>");
+
+    var settings = {
+    "url": "https://api.imgbb.com/1/upload",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": form
+    };
+
+    document.getElementById("submissionError").innerHTML = "업로드 중...";
+
+    $.ajax(settings).done(function (response) {
+        var res = JSON.parse(response);
+        if (res.success == true){
+            document.getElementById("imageURL").value = res.data.url;
+            document.getElementById("next-button").classList.remove("opacity-50");
+            document.getElementById("image").style.display = "none";
+            document.getElementById("submissionError").innerHTML = "";
+            document.getElementById("preview").src = res.data.url;
+        }
+        else{
+            document.getElementById("submissionError").innerHTML = "이미지 업로드에 실패했어요. 다시 시도해주세요.";
+        }
+    });
+});
+</script>
+</div>
+  </div>
+<?php
+include 'ui/common/footer.html.php';
+chdir(dirname(__FILE__));
+?>
--- test/view.php
+++ test/view.php
@@ -127,12 +127,14 @@ echo date("Y년 m월 d일 H:i:s", strtotime($postData["postCreation"]));
 if ($_SESSION["accessLevel"] == 5 || $_SESSION["userID"] == $postData["userID"]){
     echo'
 <div class="inline-flex mt-2 items-center rounded-md shadow-sm">
+<a href="/edit.php?id='.$_GET["id"].'">
             <button class="text-slate-800 hover:text-blue-600 text-sm bg-white hover:bg-blue-100 border border-slate-200 rounded-l-lg font-medium px-4 py-2 inline-flex space-x-1 items-center">
                 <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
                   </svg>
                   </span>
             </button>
+            </a>
             <a href="/form/postPin.php?postID='.$_GET["id"].'">
             <button class="text-slate-800 hover:text-blue-600 text-sm bg-white hover:bg-blue-100 border border-slate-200 font-medium px-4 py-2 inline-flex space-x-1 items-center">
                 <span>
