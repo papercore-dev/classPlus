ba11915c63ba84ca0e7aadf8fc482cea187f7abc
papercore-dev <gyubini38@gmail.com>
2023-03-09 18:47:36 +0000

    보안키 송수신

--- test/functions/sendNotification.php
+++ test/functions/sendNotification.php
@@ -12,29 +12,30 @@ function sendNotification($recipientUserID, $recipientSignMethod, $subject, $con
     if ($findToken_Result->rowCount() > 0){
         while($row = $findToken_Result->fetch()){
             $token = $row["token"];
+            //use Firebase Cloud Messaging API V1 to send notification
             $url = 'https://fcm.googleapis.com/fcm/send';
             $fields = array (
-                    'to' => $token,
-                    'notification' => array (
-                            "title" => $subject,
-                            "body" => $content,
-                            "icon" => $icon,
-                            "click_action" => $link
-                    )
+                'to' => $token,
+                'notification' => array (
+                    "title" => $subject,
+                    "body" => $content,
+                    "icon" => $icon,
+                    "click_action" => $link
+                )
             );
-            $fields = json_encode ( $fields );
-            $header = array (
-                    'Authorization: key=c904041bb87c3f97b41f9f9c304fe3e7dc3dbe78',
-                    'Content-Type: application/json'
+            $headers = array (
+                'Authorization: key=AAAAHuyRlw0:APA91bEK8Jtv5iNRRaDHGNRmCQd079A9njwleNXf8io5Cn0KbN55q2SteoDEgu-Iqiru5kHnrqujLXhy6SraM5ELX0j5LOEqriIozetIL1LEsGyJPhBLdwX0GpZIKJgLP6-Y4WC8rfvl',
+                'Content-Type: application/json'
             );
-            $ch = curl_init ();
-            curl_setopt ( $ch, CURLOPT_URL, $url );
-            curl_setopt ( $ch, CURLOPT_POST, true );
-            curl_setopt ( $ch, CURLOPT_HTTPHEADER, $header );
-            curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-            curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
-            $result = curl_exec ( $ch );
-            curl_close ( $ch );
+            $ch = curl_init();
+            curl_setopt( $ch,CURLOPT_URL, $url );
+            curl_setopt( $ch,CURLOPT_POST, true );
+            curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+            curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+            curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+            curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+            $result = curl_exec($ch );
+            curl_close( $ch );
             echo $result;
         }
     }
