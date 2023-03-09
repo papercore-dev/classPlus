abe40f3fd0bafaf260b4735ef587ee7d998c2683
papercore-dev <gyubini38@gmail.com>
2023-03-09 20:12:29 +0000

    테스트서버 테스트 필요 (이송조치)

--- test/dashboard/boardGenerate.php
+++ test/dashboard/boardGenerate.php
@@ -93,21 +93,21 @@ $manage_accessLevel = $_POST["manage_accessLevel"];
 
 if ($_SESSION["accessLevel"] == "5"){
     if (!isset($_POST["schoolSID"])){
-        $inviteSchoolSID = $_SESSION["schoolSID"];
+        $inviteSchoolSID = "";
     }
     else{
         $inviteSchoolSID = $_POST["schoolSID"];
     }
 
     if (!isset($_POST["schoolGrade"])){
-        $inviteSchoolGrade = $_SESSION["schoolGrade"];
+        $inviteSchoolGrade = "";
     }
     else{
         $inviteSchoolGrade = $_POST["schoolGrade"];
     }
 
     if (!isset($_POST["schoolClass"])){
-        $inviteSchoolClass = $_SESSION["schoolClass"];
+        $inviteSchoolClass = "";
     }
     else{
         $inviteSchoolClass = $_POST["schoolClass"];
