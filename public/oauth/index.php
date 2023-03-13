f9831259061975e59d35c03e24435f0db936de0f
유한선 <89729269+hanseon09@users.noreply.github.com>
2023-03-13 23:20:44 +0900

    Update index.php

--- public/oauth/index.php
+++ public/oauth/index.php
@@ -59,7 +59,7 @@ class="inline-block"></lottie-player>
 <a href="/oauth/provider/google.php?action=login">
 <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl">Google으로 시작하기</button>
 </a>
-<a href="/oauth/provider/google.php?action=login" class="w-full block mt-4 text-center text-blue-500">카카오톡으로 시작할래요 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6 inline-block">
+<a href="/oauth/provider/kakao.php?action=login" class="w-full block mt-4 text-center text-blue-500">카카오톡으로 시작할래요 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6 inline-block">
   <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"></path>
 </svg>
 </a>
