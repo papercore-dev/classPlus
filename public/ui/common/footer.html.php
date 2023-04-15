8806d4a299f3824cee1b1dbab0ad29af9d3407a9
10kwon <support@pcor.me>
2023-04-15 14:30:22 +0900

    Update footer.html.php

--- public/ui/common/footer.html.php
+++ public/ui/common/footer.html.php
@@ -4,18 +4,6 @@ include '../../security.php';
 chdir(dirname(__FILE__));?>
 <div class="pb-32"></div>
 <nav class="rounded-t-xl bottomNav shadow-lg max-w-md visible fixed bottom-0 w-full border bg-white flex overflow-x-auto">
-
-<a href="javascript:Turbo.visit(`/chat.php`, { action: 'replace' })" class="pt-2 pb-2 flex flex-col flex-grow items-center justify-center
-		overflow-hidden whitespace-no-wrap text-sm transition-colors duration-100
-		ease-in-out hover:bg-gray-200 focus:text-orange-500">
-
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
-</svg>
-
-<span class="text-sm capitalize">채팅</span>
-</a>
-
 <a href="javascript:Turbo.visit(`/calendar.php`, { action: 'replace' })" class="pt-2 pb-2 flex flex-col flex-grow items-center justify-center
 		overflow-hidden whitespace-no-wrap text-sm transition-colors duration-100
 		ease-in-out hover:bg-gray-200 focus:text-orange-500">
